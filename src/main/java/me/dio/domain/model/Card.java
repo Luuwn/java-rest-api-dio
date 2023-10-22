23e8eef8da408a11ff8b20601c3f7f1eb0a1bf1a
Luwn <luan.coast@outlook.com.br>
2023-10-22 15:01:53 -0300

    create classes

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -2,11 +2,10 @@ package me.dio.domain.model;
 
 import java.math.BigDecimal;
 
-public class Account {
+public class Card {
+
     private Long id;
     private String number;
-    private String agency;
-    private BigDecimal balance;
     private BigDecimal limit;
 
     public Long getId() {return id;}
@@ -15,12 +14,7 @@ public class Account {
     public String getNumber() {return number;}
     public void setNumber(String number) {this.number = number;}
     
-    public String getAgency() {return agency;}
-    public void setAgency(String agency) {this.agency = agency;}
-    
-    public BigDecimal getBalance() {return balance;}
-    public void setBalance(BigDecimal balance) {this.balance = balance;}
-    
     public BigDecimal getLimit() {return limit;}
     public void setLimit(BigDecimal limit) {this.limit = limit;}
+
 }
